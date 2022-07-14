9628900a314ad72530a8caf8f7c0ff26a76ec6b4
ayyn <ayyanbazil000@gmail.com>
2022-07-14 02:46:13 -0700

    updating our app with shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/homepage/shop/shop.component';
 function App() {
+  const HatsPage = () => {
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  }
   return (
     <div>
-      <HomePage/>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<HomePage/>} />
+          <Route path='/shop' element={<ShopPage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React from 'react';
+
 import "./menu-item.styles.scss"
 const MenuItem = ({title, imageUrl, size}) => {
+
   return (
     <div className={`${size} menu-item`} >
     <div className="background-image" style={{
@@ -14,4 +16,4 @@ const MenuItem = ({title, imageUrl, size}) => {
   )
 }
 
-export default MenuItem
+export default MenuItem;
