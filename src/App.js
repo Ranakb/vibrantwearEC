e4784e1a7ca0acd5b9b0c41f778736c1037075ae
ayyn <ayyanbazil000@gmail.com>
2022-07-14 03:45:53 -0700

    adding header component adn links to home,shop page and contact page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/homepage/shop/shop.component';
+import Header from './components/header/header.component';
 function App() {
-  const HatsPage = () => {
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  }
+ 
   return (
+    
     <div>
       <Router>
+      <Header/>
         <Routes>
           <Route exact path='/' element={<HomePage/>} />
           <Route path='/shop' element={<ShopPage/>} />
